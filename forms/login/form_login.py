100d1e1c3babff6a1cff2345e05ac968ad2c16aa
unknown <maximun_neo@hotmail.com>
2023-03-27 13:59:18 -0500

    feat: validacion de usuario y contraseña

--- forms/login/form_login.py
+++ forms/login/form_login.py
@@ -4,21 +4,47 @@ from tkinter.font import BOLD
 import util.generic as utl
 from forms.master.form_master import MasterPanel
 from forms.login.form_login_designer import formLoginDesigner
+from persistence.repository.auth_user_repository import AuthUserRepository
+from persistence.modelos import Auth_User
+import util.enconding_decoding as end_dec
 
 #Clase heredada que es de FormLoginDesigner
 class FormLogin(formLoginDesigner):
     
+    def __init__(self) -> None:
+        self.auth_user_repository = AuthUserRepository()
+        super().__init__()
+    
+    #Inicializacion 
+    # def __init__(self) -> None:
+    #     self.auth_repository = AuthUserRepository()
+    #     super().__init__
+    
     #Metodo Validacion de la contraseña y password
     def verificar(self):
-        usu = self.usuario.get()
-        password = self.password.get()
-        if(usu == "root" and password== "1234"):
-            self.ventana.destroy()
+        user_db: Auth_User = self.auth_user_repository.getUserByUserName(
+            self.usuario.get())
+        if(self.isUser(user_db)):
+            self.isPassword(self.password.get(), user_db)
+        
+    
+    #Metodo para verificar si es usuario
+    def isUser(self,user:Auth_User):
+        status:bool = True
+        if(user == None):
+            status = False
+            messagebox.showerror(
+            message="El ususario no existe por favor resitrarse", title="Mensaje")
+        return status
+
+
+    #Metodo para verificar si es la contraseña
+    def isPassword(self,password:str,user:Auth_User):
+        b_password = end_dec.descrypt(user.password)
+        if(password == b_password):
+            self.ventana.destroy
             MasterPanel()
         else:
-            messagebox.showerror(message="la contraseña no es correcta", title="Mensaje")
-    
-    
-    
-    def __init__(self) -> None:
-        super().__init__()
+            messagebox.showerror(message="La contraseña no es correcta", title="Mensaje")
+           
+     
--- persistence/repository/auth_user_repository.py
+++ persistence/repository/auth_user_repository.py
@@ -5,3 +5,17 @@ from sqlalchemy.orm import Session
 class AuthUserRepository():
     def __init__(self) -> None:
         self.engine= db.create_engine('mysql+pymysql://root:@localhost:3306/bd_energy_login', echo=False,future=False)
+    
+    #Validacion de usuario para verificar que este si esté contenido dentro de la base de datos    
+    def getUserByUserName(self, user_name: str):
+        user: Auth_User = None
+        with Session(self.engine) as session:
+            user = session.query(Auth_User).filter_by(
+                username=user_name).first()
+        return user
+    
+    #Insersion de usuario 
+    def inserUser(self,user:Auth_User):
+        with Session(self.engine) as session:
+            session.add(user)
+            session.commit()         
